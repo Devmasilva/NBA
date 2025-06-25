6ee88e6ce779cdd449eec8df7f72fa7d424289ef
Matheus Cavalcante da Silva <117126309+Devmasilva@users.noreply.github.com>
2025-06-24 21:15:06 -0300

    Update aula3CalculoMedia.js

--- aula3CalculoMedia.js
+++ aula3CalculoMedia.js
@@ -4,6 +4,8 @@ function calcularMedia(primeiroNumero, segundoNumero, terceiroNumero) {
   const resultadoDaMedia = somaDosTresValores / 3
   return resultadoDaMedia
 }
+// retur (primeiroNumero + segundoNumero + terceiroNumero) / 3
+//Uma forma para dar o retorno mais rapido 
 
 // Usar a Função!
 const resultadoDaMediaDoMarcio = calcularMedia(5, 7, 9)
