71dd8aa5dc96ac035de3f3daf9421210f135930c
Matheus Cavalcante da Silva <117126309+Devmasilva@users.noreply.github.com>
2025-06-24 21:12:04 -0300

    Create aula3CalculoMedia.js

---
+++ aula3CalculoMedia.js
@@ -0,0 +1,13 @@
+// Desenhando a Função!
+function calcularMedia(primeiroNumero, segundoNumero, terceiroNumero) {
+  const somaDosTresValores = primeiroNumero + segundoNumero + terceiroNumero
+  const resultadoDaMedia = somaDosTresValores / 3
+  return resultadoDaMedia
+}
+
+// Usar a Função!
+const resultadoDaMediaDoMarcio = calcularMedia(5, 7, 9)
+console.log('Média do Márcio: ' + resultadoDaMediaDoMarcio)
+
+const resultadoDaMediaDaBea = calcularMedia(4, 8, 12)
+console.log('Média da Bea: ' + resultadoDaMediaDaBea)
